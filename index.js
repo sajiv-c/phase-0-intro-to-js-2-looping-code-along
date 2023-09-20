61f3d299020a30f203b5141bb153aa68f26806bd
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-09-20 14:43:17 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const newNames = [];
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newNames;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
